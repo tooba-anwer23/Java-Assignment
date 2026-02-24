8237656e7af93f42eea837c3d5122f5e6427b4e5
tooba-anwer23 <toobaanwer642@gmail.com>
2026-02-24 16:02:25 +0500

    done

---
+++ chapter 2/app.js
@@ -0,0 +1,48 @@
+// answer 1
+
+// let userName = "TOOBA"
+
+// answer 2
+
+// let myName = "Tooba Anwer"
+
+// answer 3
+
+// let massage = "Hello World"
+// alert (massage)
+
+// answer 4
+
+// let studentName = "Tooba Anwer"
+// alert (studentName)
+
+// let studentAge = 20
+// alert ( studentAge  + " Years Old")
+
+// let studentCourse = "Modern Web And App Development"
+// alert (studentCourse)
+
+// answer 5
+
+// let content = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(content)
+
+// answer 6
+
+// let email = "toobaanwer642@gmail.com"
+// alert ( "My email is " + email)
+
+// answer 7
+
+// let book = "A smarter way to learn Javascript"
+// alert ("I am trying to learn from the book " + book)
+
+
+// answer 8
+
+// document.write ("Yah! I can write HTML content through Javascript")
+
+// answer 9
+
+let pattern = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert (pattern)
