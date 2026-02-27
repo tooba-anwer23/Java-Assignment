e08865ae36de0606095f5d342f7e3a714bacced8
tooba-anwer23 <toobaanwer642@gmail.com>
2026-02-27 22:05:57 +0500

    done

---
+++ chapter 1/app.js
@@ -0,0 +1,21 @@
+// answer 1
+
+// alert ("Welcome! To My Website ")
+
+// answer 2
+
+// alert ("Error! Please enter a valid Password")
+
+// answer 3
+
+// alert ("First! enter your age before start Coding")
+
+// answer 4
+
+// alert ("Welcome To Js Land")
+
+// alert ("Happy Coding")
+
+// answer 5
+
+// alert ("Hello.... I can run JS through my web browser's console ")
