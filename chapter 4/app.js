15d31b0b22b5f7ce0c2bcb22c0f2309c12c04478
tooba-anwer23 <toobaanwer642@gmail.com>
2026-03-12 00:03:17 +0500

    done

---
+++ chapter 4/app.js
@@ -0,0 +1,35 @@
+// answer 1
+
+// let a,b,c 
+
+// answet 2
+
+// let nameOfStudent = "Tooba"
+// let _Name = "Tooba"
+// let $Name = "Tooba"
+// let Name1 = "Tooba"
+// let studentName = "Tooba"
+
+// let 1name = "Tooba"
+// let @name = "Tooba"
+// let student Name = "Tooba"
+// let var = "Tooba"
+// let student-Name = "Tooba"
+
+// answer 3
+
+// part a
+// document.write("<h2>Rules for naming Js variables</h2>")
+// document.write("<br></br>")
+// // part b
+// document.write("Variable names can only contain numbers, $, _. For example: $My_1st Variable")
+// document.write("<br></br>")
+// // part c
+// document.write("Variables must begin with a letter, $, or _ . For example $name, _name, or name")
+// document.write("<br></br>")
+// // part d
+// document.write("Variable names are case Sensitive")
+// document.write("<br></br>")
+// // part e
+// document.write("Variable names should not be JS Keywords")
+
