d5f991e0b0d9be76b11fa67491f4d85970d95e94
tooba-anwer23 <toobaanwer642@gmail.com>
2026-03-11 21:22:04 +0500

    done

---
+++ chapter 3/app.js
@@ -0,0 +1,14 @@
+// answer 1
+
+// let age = 20
+// alert ("I am " + age + " years old ")
+
+// answer 2
+
+// let visit = 15
+// alert (" you have visited this site " + visit + " times ") 
+
+// answer 3
+
+// let birthYear = 2005
+// document.write ("My birth year is " + birthYear)
